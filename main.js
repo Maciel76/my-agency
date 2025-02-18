87ccc8017d30bd2aab1cc8ac1e1db0e6b9d0ebed
Maciel <debuggenius@hotmail.com>
2025-02-18 01:40:28 -0300

    Criação das pages header e footer

--- main.js
+++ main.js
@@ -42,3 +42,14 @@ const statsSection = document.querySelector('.stats');
 if (statsSection) {
     observer.observe(statsSection);
 }
+ // Função para carregar componentes HTML
+ function loadComponent(id, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.getElementById(id).innerHTML = data)
+        .catch(error => console.error(`Erro ao carregar ${url}:`, error));
+}
+
+// Carregar o header e footer
+loadComponent("header", "header.html");
+loadComponent("footer", "footer.html");s
---
+++ public/router/main.js
@@ -0,0 +1,11 @@
+ // Função para carregar componentes HTML
+ function loadComponent(id, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.getElementById(id).innerHTML = data)
+        .catch(error => console.error(`Erro ao carregar ${url}:`, error));
+}
+
+// Carregar o header e footer
+loadComponent("header", "header.html");
+loadComponent("footer", "footer.html");
